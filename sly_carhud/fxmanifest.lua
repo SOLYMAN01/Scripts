30b69c2668100e99d3219659b153050690db480c
SOLYMAN <hugogs0799@gmail.com>
2023-01-07 17:43:06 +0100

    sly_carhud

---
+++ sly_carhud/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author 'LeakMaster'
+description 'LeakMaster Carhud'
+
+ui_page 'html/index.html'
+
+files {
+	'html/index.html',
+	'html/index.js',
+	'html/style.css',
+	'html/fonts/*ttf',
+}
+
+client_scripts {
+	'client/main.lua',
+}
